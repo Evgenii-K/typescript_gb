e7be76f310787b207c73033b320887b883ae1970
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-11-03 03:01:00 +0500

    completed task 1

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,4 +1,6 @@
 import { renderBlock, GetCheckDate } from './lib.js'
+import { renderEmptyOrErrorSearchBlock, renderSearchResultsBlock } from './search-results.js';
+
 
 interface SearchFormData {
   city: string;
@@ -7,13 +9,13 @@ interface SearchFormData {
   price: number;
 }
 
-interface Place {
+export interface Places {
   id: string;
-  title: string;
-  details: string;
-  photos: Array<string>;
-  coordinates: Array<number>;
-  bookedDates: Array<any>;
+  name: string;
+  description: string;
+  image?: string;
+  remoteness: number;
+  bookedDates?: Array<any>;
   price: number
 }
 
@@ -64,19 +66,24 @@ export function renderSearchFormBlock (): void {
   )
 }
 
-export function search (result: (data: SearchFormData) => void): void {
+export function search (result: (data: SearchFormData) => void, cb: (res: string | Places[]) => void) {
   const form = document.querySelector('form');
-  const city: string = (<HTMLInputElement>document.querySelector('#city')).value;
-  const checkInDate: string = (<HTMLInputElement>document.querySelector('#check-in-date')).value;
-  const checkOutDate: string = (<HTMLInputElement>document.querySelector('#check-out-date')).value;
-  const price: number = +(<HTMLInputElement>document.querySelector('#max-price')).value;
-
 
   if (form != null) {
     form.addEventListener('submit', (event) => {
       event.preventDefault();
 
+      const city: string = (<HTMLInputElement>document.querySelector('#city')).value;
+      const checkInDate: string = (<HTMLInputElement>document.querySelector('#check-in-date')).value;
+      const checkOutDate: string = (<HTMLInputElement>document.querySelector('#check-out-date')).value;
+      const price: number = +(<HTMLInputElement>document.querySelector('#max-price')).value;
+      
       result({city, checkInDate, checkOutDate, price})
+
+      fetch('/places')
+      .then(res => res.json())
+      .then(data => cb(data))
+      .catch(err => cb(err))
     })
   } else {
     result({city: '', checkInDate: '', checkOutDate: '', price: 0})
@@ -85,4 +92,17 @@ export function search (result: (data: SearchFormData) => void): void {
 
 export function getResult (enteredData: SearchFormData): void {
   console.log(enteredData)
+}
+
+export function getPlaces (res: string | object): void {
+
+  if(typeof res === 'string') {
+    renderEmptyOrErrorSearchBlock(res)
+  } else {
+    let arr = []
+    for(let key in res) {
+      arr.push(res[key])
+    }
+    renderSearchResultsBlock(arr)
+  }
 }
